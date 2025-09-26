7a3dc8d67e9fc3288fc735987810f75288bbb657
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-09-26 10:16:34 -0300

    adicionado colaborador module

---
+++ src/colaborador/colaborador.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module{
+  imports: [TypeOrmModule.forFeature([Colaborador])],
+  controllers: [ColaboradorController],
+  providers: [ColaboradorService],
+  exports: [],
+}
+export class ColaboradorModule {}
