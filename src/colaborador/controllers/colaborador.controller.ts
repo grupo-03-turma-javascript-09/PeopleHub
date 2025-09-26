61084015519a0d38f47ade2de93e14c4a1208ff9
Joe Chriszel Silva <90300759+JoeChriszelSilva@users.noreply.github.com>
2025-09-26 10:38:56 -0300

    controllers

--- src/colaborador/controllers/colaborador.controller.ts
+++ src/colaborador/controllers/colaborador.controller.ts
@@ -1,1 +1,14 @@
-export class ColaboradorController {}
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Colaborador } from '../entities/colaborador.entity';
+import { ColaboradorService } from '../services/colaboradores.service';
+
+@Controller('/colaborador')
+export class ColaboladoresController {
+  constructor(private readonly colaboradoresService: ColaboradoService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.ok)
+  findAll(): Premise<Colaborador[]> {
+    return this.colaboradoresService.findAll();
+  }
+}
